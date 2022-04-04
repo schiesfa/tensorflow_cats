45b87d541ef29b7eaf39b33daf66c688a0d05197
schiesfa <fabienne-celine.schiesser@hs-augsburg.de>
2022-04-04 20:26:12 +0200

    made tensorflow model

---
+++ main.py
@@ -0,0 +1,133 @@
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+data_dir_train = 'images/Training'
+data_dir_test = 'images/Test'
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir_train,
+  seed=123,
+  image_size=(180, 180),
+  batch_size=32)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir_test,
+  seed=123,
+  image_size=(180, 180),
+  batch_size=32)
+
+for images,labels in train_ds:
+    images = images / 255.0
+
+for images,labels in val_ds:
+    images = images / 255.0
+
+
+class_names = train_ds.class_names
+plt.figure(figsize=(10, 10))
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+    plt.colorbar()
+
+plt.show()
+
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+num_classes = 2
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=20
+)
+
+test_loss, test_acc = model.evaluate(val_ds, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+probability_model = tf.keras.Sequential([model,tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(val_ds.take(1))
+
+predictions[0]
+
+np.argmax(predictions[0])
+
+def plot_image(i, predictions_array, dataset):
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  for images, labels in dataset.take(i + 1):
+      image = images[i]
+      true_label = labels[i]
+
+  plt.imshow(image.numpy().astype("uint8"))
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+  plt.subplot(3, 2 * 3, 2 * i + 2)
+
+  plt.grid(False)
+  plt.xticks(range(2))
+  plt.yticks([])
+  thisplot = plt.bar(range(2), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+
+num_rows = 3
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], val_ds)
+plt.tight_layout()
+plt.show()
+
+
+
+
+
